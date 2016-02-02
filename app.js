0a1e587d13e0cdbeae0aaac8323e9754818f8b21
pascalhelbig <pascalhelbig@gmail.com>
2016-02-02 16:54:05 +0100

    add filesize route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ app = express();
 
 app.use(express.static('public'));
 
+app.post('/api/fileanalyse', function(req, res) {
+  res.json({fileSize: 1337});
+});
+
 app.listen(8080, function () {
   console.log('Server running on 8080');
 });
