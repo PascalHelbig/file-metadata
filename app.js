37630d3f70af95f94467ad6c98643f0831fd4c78
pascalhelbig <pascalhelbig@gmail.com>
2016-02-02 17:32:58 +0100

    return uploaded filesize

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var multer = require('multer')();
 app = express();
 
 app.use(express.static('public'));
 
-app.post('/api/fileanalyse', function(req, res) {
-  res.json({fileSize: 1337});
+app.post('/api/fileanalyse', multer.single('0'), function (req, res) {
+  if (req.file) {
+    res.json({fileSize: req.file.size});
+  } else {
+    res.status(404).send('File not found');
+  }
 });
 
 app.listen(8080, function () {
